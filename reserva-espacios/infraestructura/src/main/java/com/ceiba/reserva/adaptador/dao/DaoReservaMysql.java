3c2f89d85553446fcf467bdff0613c69a5f86b6c
Wilmer Escobar <wilmer.escobar@ceiba.com.co>
2021-06-23 23:24:01 -0500

    Se quita smell en el código

--- reserva-espacios/infraestructura/src/main/java/com/ceiba/reserva/adaptador/dao/DaoReservaMysql.java
+++ reserva-espacios/infraestructura/src/main/java/com/ceiba/reserva/adaptador/dao/DaoReservaMysql.java
@@ -1,12 +1,8 @@
 package com.ceiba.reserva.adaptador.dao;
 
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Component;
-
 import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
 import com.ceiba.reserva.modelo.dto.DtoReserva;
@@ -16,7 +12,6 @@ import com.ceiba.reserva.puerto.dao.DaoReserva;
 public class DaoReservaMysql implements DaoReserva {
 
     private final CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate;
-    private static final Logger LOGGER = LoggerFactory.getLogger(DaoReservaMysql.class);
 
     @SqlStatement(namespace="reserva", value="listar")
     private static String sqlListar;
